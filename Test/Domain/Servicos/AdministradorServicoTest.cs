5fc501a07196d188408a1d1c9c9f0fa422bacd2f
Jess <jessica.sileite@gmail.com>
2025-08-05 16:35:20 -0300

    teste servico administrador

---
+++ Test/Domain/Servicos/AdministradorServicoTest.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MinimalApi.Dominio.Entidades;
+using MinimalApi.Dominio.Servicos;
+using MinimalApi.Infraestrutura.Db;
+
+namespace Test.Domain.Entidades;
+
+[TestClass]
+public class AdministradorServicoTest
+{
+    private DbContexto CriarContextoDeTeste()
+    {
+        var assemblyPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        var path = Path.GetFullPath(Path.Combine(assemblyPath?? "","..","..",".."));
+        
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(path?? Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        return new DbContexto(builder.Build());
+    }
+
+    [TestMethod]
+    public void TestandoSalvarAdministrador()
+    {
+        // Arrange
+        var context = CriarContextoDeTeste();
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE Administradores");
+
+        var administrador = new Administrador
+        {
+            Email = "admin@test.com",
+            Senha = "123456",
+            Perfil = "Adm"
+        };
+
+        var administradorServico = new AdministradorServico(context);
+
+        // Act
+        administradorServico.Incluir(administrador);
+
+        // Assert
+        Assert.AreEqual(1, administradorServico.Todos(1).Count);
+    }
+}
